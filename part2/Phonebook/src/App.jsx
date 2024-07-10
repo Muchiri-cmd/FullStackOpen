524b678756fa37bcb6b7856b8d340b67b1570187
Muchiri-cmd <davismuchiri21@gmail.com>
2024-07-11 01:06:39 +0300

    Add Exercises 2.12.-2.15 - (Phonebook) solution

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState,useEffect } from "react";
-import axios from 'axios'
+import personServices from './services/persons'
 
 const Filter = ({value,onChange}) => {
   return (
@@ -31,27 +31,37 @@ const Form = ({name,handleNameField,number,handleNumberField,onSubmit}) => {
     </form>
   )
 }
-const Persons  = ({list}) => {
+const Persons  = ({list,deleteRecord}) => {
   return(
     <>
       {list.map(person => 
-        <Person key={person.id} name={person.name} number={person.phoneNumber}/>
+        <Person key={person.id} id={person.id} name={person.name} number={person.phoneNumber} deleteRecord={deleteRecord}/>
       )}
     </>
   )
 }
-const Person = ({name,number}) => <p>{name} - {number}</p>
+const Person = ({name,number,id,deleteRecord}) => {
+  return (
+    <div style={{margin: 1.5 + 'em'}}>
+      <span>{name} - {number}</span>
+      <button 
+        style={{marginLeft: 1.5 + 'em'}}
+        onClick={() => deleteRecord(id)}
+        >Delete
+      </button>
+    </div>
+  )
+} 
 
 const App = () => {
   const[persons,setPersons] = useState([])
 
   useEffect(()=>{
-    axios
-      .get('http://localhost:3001/persons')
-      .then(res => {
-        console.log(res.data);
-        setPersons(res.data)
-        setFilteredPersons(res.data)
+    personServices
+      .getPeople()
+      .then(peopleData => {
+        setPersons(peopleData)
+        setFilteredPersons(peopleData)
       })
   },[])
 
@@ -75,25 +85,67 @@ const App = () => {
   }
 
   const handleSubmit = (event) => {
-    // console.log('Submitting ---');
     event.preventDefault()
+    const generatedId =  String(persons.length ? Math.max(...persons.map(p => p.id)) + 1 : 1)
     
     const newPerson = { 
-      name:newName,
-      phoneNumber:newNumber,
-      id: persons.length ? Math.max(...persons.map(p => p.id)) + 1 : 1
+      name:newName.trim(),
+      phoneNumber:newNumber.trim(),
+      id: generatedId
     }
     const exists = persons.find(person => person.name === newName)
+
     if (exists) {
-      alert(`${newName} is already added to phonebook, try another name`)
-      setNewName('')
-      return
-    } else {
-      setPersons(persons.concat(newPerson))
-      setFilteredPersons(persons.concat(newPerson))
+      const id = exists.id
+      const person = persons.find(p => p.id === id )
+      const updatedPerson = {...person,phoneNumber:newNumber}
+
+      window.confirm(`${newName} is already added to phonebook, replace old number with a new one ?`)
+
+      personServices
+        .updateRecord(id,updatedPerson)
+        .then(returnedPerson =>{
+           setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+           setFilteredPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+        })
       setNewName('')
       setNewNumber('')
+  
+    } else {
+      personServices
+        .addPerson(newPerson)
+        .then(personData => {
+          setPersons(persons.concat(personData))
+          setFilteredPersons(persons.concat(personData))
+          setNewName('')
+          setNewNumber('')
+        }).catch(err => {
+          console.log(err);
+          alert('Error adding person');
+        });
     }
+  }
+
+  const deleteRecord = (id) => {
+   
+    const person = persons.find(p => p.id === id)
+    window.confirm(`Are you sure you want to delete ${person.name}'s record`)
+    
+    personServices
+      .deletePerson(id)
+      .then(() => {
+        const updatedPersons = persons.filter(p => p.id !== id);
+        setPersons(updatedPersons);
+        setFilteredPersons(updatedPersons);
+
+      }).catch(err => {
+        console.log(err);
+        // alert(`The person ${person.name} was already deleted from server`);
+        alert(`Couldn't delete ${person.name}'s record . Check his record for errors `)
+        const updatedPersons = persons.filter(p => p.id !== id);
+        setPersons(updatedPersons);
+        setFilteredPersons(updatedPersons);
+      });
   }
 
   return (
@@ -102,8 +154,6 @@ const App = () => {
       <Filter value={searchVal} onChange={handleSearch}/>
       
       <h3>Add a new</h3>
-
-
       <Form 
         onSubmit={handleSubmit}
         name={newName}
@@ -111,10 +161,9 @@ const App = () => {
         number={newNumber}
         handleNumberField={handleNumberField}
       />
-     
 
       <h2>Numbers</h2>
-      <Persons list={filteredPersons}/>
+      <Persons list={filteredPersons} deleteRecord={deleteRecord}/>
     </>
   )
 }
