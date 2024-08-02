c8cf46da61f9fa8f1bcf902401e334504022bf1c
Muchiri-cmd <davismuchiri21@gmail.com>
2024-08-02 12:39:46 +0300

    Add style & button to each blog to toggle details (Ex 5.7 )

--- part5/Blog List/bloglist-frontend/src/components/Blog.jsx
+++ part5/Blog List/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}, {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+
+  const [visible,setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hide = { display:visible ? 'none': ''}
+  const show = { display:visible ? '' : 'none'}
+  
+  return(
+    <>
+        <div style={{ ...blogStyle, ...hide }}>
+          {blog.title}, {blog.author}
+          <button onClick={ () => setVisible(true)}>View</button>
+        </div>  
+        <div style={{ ...blogStyle, ...show }}>
+          <p>{blog.title}
+            <button onClick={ () => setVisible(false)}>hide</button>
+          </p>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.author}</p>
+        </div> 
+    </>
+
+
+  )
+  
+}
 
 export default Blog
