c3d73923435c75166c9eae59d5dcd1d3210e5b54
Muchiri-cmd <davismuchiri21@gmail.com>
2024-11-13 06:58:32 +0300

    (Ex 8.19) - Complete application to filter the book list by genre.

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,12 @@
 import { useQuery } from "@apollo/client"
 import { ALL_BOOKS } from "../queries/queries"
+import { useState } from "react"
 
 const Books = (props) => {
   const books = useQuery(ALL_BOOKS)
  
+  const [filteredBooks,setFilteredBooks] = useState(null)
+
   if(books.loading){
     return <div>Loading...</div>
   }
@@ -11,6 +14,15 @@ const Books = (props) => {
   if (!props.show) {
     return null
   }
+
+  const allGenres = [...new Set (books.data.allBooks.flatMap(book => book.genres))]
+  
+  const filter = (genre) => {
+    const filtered = books.data.allBooks.filter(book => book.genres.includes(genre))
+    setFilteredBooks(filtered)
+  }
+
+  const displayBooks = filteredBooks || books.data.allBooks
 
   return (
     <div>
@@ -23,7 +35,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((b) => (
+          {displayBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -32,6 +44,12 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      
+      <button onClick={() => setFilteredBooks(null)}>All</button>
+      {allGenres.map((genre,index) => (
+        <button key={index} onClick={() => filter(genre)}>{genre}</button>
+      ))}
+     
     </div>
   )
 }
--- part8/library-frontend/src/main.jsx
+++ part8/library-frontend/src/main.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import { ApolloClient,InMemoryCache,ApolloProvider } from "@apollo/client";
+import { ApolloClient,InMemoryCache,ApolloProvider,createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('user-token')
+
+  return {
+    headers:{
+      ...headers,
+      authorization:token ? `Bearer ${token}`:null
+    }
+  }
+})
+
+const httpLink = createHttpLink({
+  uri:'http://localhost:4000/'
+})
 
 const client = new ApolloClient({
-  uri:'http://localhost:4000',
+  link:authLink.concat(httpLink),
   cache:new InMemoryCache(),
 })
 
