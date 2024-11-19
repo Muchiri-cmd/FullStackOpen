01db751cb855f38aa9aa95166cf06e43d66b00ab
Muchiri-cmd <davismuchiri21@gmail.com>
2024-11-19 11:46:49 +0300

    (Ex 9.9) - Patientor backend   - Fork and clone the project patientor   - Ensure ping request that frontend has are made on startup (add cors)   - Add a patients endpoint

--- part9/Patientor/backend/build/index.js
+++ part9/Patientor/backend/build/index.js
@@ -4,12 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = 8000;
+app.use((0, cors_1.default)());
+const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     console.log('ping!!!');
     res.send('pong');
+});
+app.get('/api/patients', (_req, _res) => {
+    console.log('fetching patients');
 });
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
--- part9/Patientor/backend/index.ts
+++ part9/Patientor/backend/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const PORT = 8000;
+const PORT = 3001;
 
 app.get('/api/ping',(_req,res) => {
   console.log('ping!!!');
   res.send('pong');
+});
+
+app.get('/api/patients',(_req,_res) => {
+  console.log('fetching patients');
 });
 
 app.listen(PORT,()=>{
